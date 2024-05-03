9ac53f3a34ff78695b95af0ef328609eaab0d646
MAR-tdf454 <100684900+MAR-tdf454@users.noreply.github.com>
2024-05-03 13:17:57 +0200

    Authored by Omid Nadri

---
+++ Breakout/BreakoutBus.cs
@@ -0,0 +1,12 @@
+//using DIKUArcade.EventBus;
+
+using DIKUArcade.Events;
+
+namespace Breakout {
+    public static class BreakoutBus {
+        private static GameEventBus? eventBus;
+        public static GameEventBus GetBus() {
+            return BreakoutBus.eventBus ?? (BreakoutBus.eventBus = new GameEventBus());
+        }
+    }
+}
